6069a201e3d70bb40e1e8b0b9aca45cf5e4902b7
yusufadell <youseefadel777@gmail.com>
2022-11-07 21:40:02 +0200

    feat: rename project to sqwoz

--- sqwoz/asgi.py
+++ sqwoz/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for valuable project.
+ASGI config for Sqwoz project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "valuable.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sqwoz.settings")
 
 application = get_asgi_application()
