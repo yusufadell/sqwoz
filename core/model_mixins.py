0e38654e740b4d1f96495deb39b90f7be5630298
Restyled.io <commits@restyled.io>
2022-11-07 19:50:35 +0000

    Restyled by isort

--- core/model_mixins.py
+++ core/model_mixins.py
@@ -1,8 +1,6 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-
 from django.utils.translation import gettext_lazy as _
-
-from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class DiscountMixin(models.Model):
--- products/models.py
+++ products/models.py
@@ -1,7 +1,7 @@
 from django.db import models
+from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
-from django.urls import reverse
 from core.model_mixins import DiscountMixin
 
 
